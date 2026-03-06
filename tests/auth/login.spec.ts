f7b4ecffdc69b87000d49e8653877f9b230bf90e
Tống Đức Quang Hưng <quanghung@MacBook-Air-cua-Tong.local>
2026-03-06 11:22:54 +0700

    TC02 - Login with invalid password

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -12,4 +12,12 @@ test.describe('Authentication - Login', () => {
         await expect(page).toHaveURL(/inventory.html/);
     });
 
+    test('TC02 - Login with wrong password', async ({page}) => {
+        const loginPage = LoginPage(page);
+        await page.goto('/')
+        await loginPage.enterUserName('standard_user');
+        await loginPage.enterPassword('!@#$$');
+        await loginPage.clickLogin();
+        await expect(page.getByTestId('error')).toBeVisible();
+    });
 });
