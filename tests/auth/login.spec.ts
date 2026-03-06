56e3aee44889a6974527cfca0ecc35f4e79fcd1f
Tống Đức Quang Hưng <quanghung@MacBook-Air-cua-Tong.local>
2026-03-06 12:04:42 +0700

    fix

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -12,4 +12,22 @@ test.describe('Authentication - Login', () => {
         await expect(page).toHaveURL(/inventory.html/);
     });
 
+        test('TC02 - Login with wrong password', async ({page}) => {
+        const loginPage = LoginPage(page);
+        await page.goto('/')
+        await loginPage.enterUserName('standard_user');
+        await loginPage.enterPassword('!@#$$');
+        await loginPage.clickLogin();
+        await expect(page.getByTestId('error')).toBeVisible();
+    });
+
+      test('TC03 - Login with locked user', async ({page}) => {
+        const loginPage = LoginPage(page);
+        await page.goto('/')
+        await loginPage.enterUserName('locked_out_user');
+        await loginPage.enterPassword('secret_sauce');
+        await loginPage.clickLogin();
+        await expect(page.getByTestId('error')).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+    });
 });
+
